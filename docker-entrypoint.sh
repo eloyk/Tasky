9acb32761fdf5bddd5a350226058c11c5bdd1781
eloyk <stifler9.0@hotmail.com>
2025-11-20 18:56:46 +0000

    ci: wip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 echo "Running database migrations..."
-npx drizzle-kit push --force
+#npx drizzle-kit push --force
+npm run db:push --force
 
 echo "Starting application..."
 exec node dist/server/index.js
